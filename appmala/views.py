133d4e3b5231f1791df564739b1badbb6ab417ca
Sohyun Eom <sohy19@hufs.ac.kr>
2021-12-10 01:37:31 +0900

    [update] comment

--- appmala/views.py
+++ appmala/views.py
@@ -38,12 +38,12 @@ def home(request):
 def detail(request, id):
     store = get_object_or_404(Store, pk = id)
     reviews = Review.objects.filter(store=id)
-    comments = Comment.objects.filter(comment_id=id)
-    return render(request, 'detail.html', {'store': store, 'reviews': reviews, 'comments': comments})
+    # comments = Comment.objects.filter(comment_id=id)
+    return render(request, 'detail.html', {'store': store, 'reviews': reviews})
 
 def review(request, id):
     review = get_object_or_404(Review, pk = id)
-    comments = Comment.objects.filter(comment_id=id)
+    comments = Comment.objects.filter(review=id)
     return render(request, 'review.html', {'review': review, 'comments': comments})
 
 def newstore(request):
@@ -66,6 +66,11 @@ def create(request):
         return redirect('appmala:detail', new_store.id)
     return redirect('home')
 
+def delete(request, id):
+    store = Store.objects.get(id=id)
+    store.delete()
+    return redirect("home")
+
 def createReview(request, store_id):
     form = ReviewForm(request.POST, request.FILES)
     item =  get_object_or_404(Store, pk = store_id)
@@ -80,28 +85,24 @@ def createReview(request, store_id):
         return redirect('appmala:review', new_review.id)
     return redirect('home')
 
-def delete(request, id):
-	store = Store.objects.get(id=id)
-	store.delete()
-	return redirect("home")
+def deleteReview(request, id):
+    review = Review.objects.get(id=id)
+    review.delete()
+    return redirect("appmala:detail", review.store_id)
 
 def create_comment(request):
     if request.method == "POST":
         comment = Comment()
         comment.comment_content = request.POST.get('comment_content', '')
-        comment.review_id = Review.objects.get(pk=request.POST.get('review_id'))
+        comment.review = Review.objects.get(pk=request.POST.get('reveiw_id'))
         writer = request.user
         print(writer)
         if writer:
             comment.user = CustomUser.objects.get(username=writer)
         else:
-            return redirect('appmala:detail', comment.review_id.id)
+            return redirect('appmala:review', comment.review_id)
         comment.comment_date = timezone.now()
         comment.save()
-        return redirect('appmala:detail', comment.review_id.id)
+        return redirect('appmala:review', comment.review_id)
     else:
         return redirect('home')
-def deleteReview(request, id):
-   review = Review.objects.get(id=id)
-   review.delete()
-   return redirect("appmala:detail", review.store_id)
